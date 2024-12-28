43ba11c07d4176de2239b5d1c7fdeaf73d477a03
pullemalavamsi <pullemalavamsi@gmail.com>
2024-12-28 09:35:03 +0000

    TRAVELGUIDE

--- src/TravelGuide/index.js
+++ src/TravelGuide/index.js
@@ -1,15 +1,15 @@
-import './index.css';
+import './index.css'
 
-const TravelGuide = (props) => {
-  const { details } = props;
-  const { name, imageUrl, description } = details;
+const TravelGuide = props => {
+  const {details} = props
+  const {name, imageUrl, description} = details
   return (
     <li className="li-ei">
       <img src={imageUrl} className="ima" alt={name} />
       <h1 className="name">{name}</h1>
       <p className="desc">{description}</p>
     </li>
-  );
-};
+  )
+}
 
-export default TravelGuide;
+export default TravelGuide
